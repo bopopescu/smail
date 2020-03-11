283c3f6cad1e428dc681b7052e9b54549b33dd99
fancuiyi <240577132@qq.com>
2020-03-11 20:45:17 +0800

    家里发到单位

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrator.SC-201905091754\Desktop\发送邮件\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
